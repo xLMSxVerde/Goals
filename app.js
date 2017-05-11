8bddcce249dbe37d466d9a7effb55b0cfa05b6ae
xLMSxVerde <benedictkempkes@googlemail.com>
2017-05-12 00:09:14 +0200

    auth
    setup

---
+++ app.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var morgan = require('morgan');
+
+var favicon = require('serve-favicon');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var configDB = require('./config/database.js');
+
+mongoose.connect(configDB.url); //connect to our db
+
+require('./config/passport')(passport); // pass passport for configuration
+
+
+
+// view engine setup
+app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
+app.use(bodyParser());
+app.use(cookieParser());
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(port);
+console.log('The magic happens on port: ' + port);
+
+module.exports = app;
---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://root:Start2016!@ds137191.mlab.com:37191/firstdb'
+
+};
